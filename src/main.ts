6d90b7eff7b35bfa7f33d0d7b5bce61325604fd2
Your Name <diablosabrith@gmail.com>
2025-11-15 20:51:21 +0500

    corse debug

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module'; // без .js
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Включаем CORS
+  // Разрешаем локальный фронт + твой Vercel фронт
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://sonnik-alrtxwb92-diablosabriths-projects.vercel.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
   });
- await app.listen(process.env.PORT ?? 8000);
- 
+
+  await app.listen(process.env.PORT ?? 8000);
 }
+
 bootstrap();
